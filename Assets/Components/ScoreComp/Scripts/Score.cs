d09793585b636ad0e851ba1e34fd09f8051fade4
Sceezee <macsum200311@gmail.com>
2020-11-28 01:46:07 +0200

    i dont give a fuck

---
+++ Assets/Components/ScoreComp/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // i dont give a fuck
+        // i dont give a fuck
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
