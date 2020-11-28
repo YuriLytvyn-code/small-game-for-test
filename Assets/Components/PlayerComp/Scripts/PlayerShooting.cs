2971c19a9cf8d53ed880d49a40dba9bdd8253a29
senoron <perfectbynni@gmail.com>
2020-11-28 14:03:56 +0200

    shooting

---
+++ Assets/Components/PlayerComp/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    float ShootColldow = 0;
+    public float fireDelay = 0.5f;
+
+    public GameObject bullet; 
+    void Update()
+    {
+        ShootColldow -= Time.deltaTime;
+        if(Input.GetKey(KeyCode.Mouse0) && ShootColldow <= 0)
+        {
+            Debug.Log("Pey!");
+            ShootColldow = fireDelay;
+            
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+
+    }
+}
---
+++ Assets/Components/PlayerComp/Scripts/SelfDistruct.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDistruct : MonoBehaviour
+{
+    public float lifeTime = 5f;
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0){Destroy(gameObject);}
+    }
+}
