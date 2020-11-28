d783667db788ad874d42e03e5fe0cecf46c764f1
senoron <perfectbynni@gmail.com>
2020-11-28 12:14:31 +0200

    added stop button

--- Assets/Components/PlayerComp/Scripts/PlayerMovement.cs
+++ Assets/Components/PlayerComp/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveForce = 5f;
+    public float moveForce = 50f;
+    public float stopSpeed = 0.9889f;
     Rigidbody2D rb;
     Vector2 movement;
     void Start()
@@ -16,15 +17,28 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if(Input.GetKey(KeyCode.Space))
+        {
+            stopPlayer(movement);
+        }
     }
 
     void FixedUpdate()
     {
         movePlayer(movement);
+        // if(Input.GetKey(KeyCode.Space))
+        // {
+        //     stopPlayer(movement);
+        // }
     }
 
     void movePlayer(Vector2 direction)
     {
         rb.AddForce(direction * moveForce * Time.deltaTime);
+    }
+
+    void stopPlayer(Vector2 direction)
+    {
+        rb.velocity *= stopSpeed;
     }
 }
