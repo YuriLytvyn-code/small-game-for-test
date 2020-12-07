dfa237cc954da4fa97b14068478df1e953559ee5
senoron <perfectbynni@gmail.com>
2020-12-07 15:40:18 +0200

    Fixed meteors animation

--- Assets/Components/Meteors/Scripts/MeteorController.cs
+++ Assets/Components/Meteors/Scripts/MeteorController.cs
@@ -7,7 +7,9 @@ public class MeteorController : MonoBehaviour
     [SerializeField] private float meteorHP = 200f;
     [SerializeField] private float currentHP = 0f;
     [SerializeField] private float meteorDamage = 100f;
-
+    [SerializeField] private int ironKoef = 1;
+    [SerializeField] private int goldKoef = 5;
+    [SerializeField] private int diamondKoef = 10;
     private Animator anim;
     void Start()
     {
@@ -28,6 +30,10 @@ public class MeteorController : MonoBehaviour
     {
         gameObject.GetComponent<CircleCollider2D>().enabled = false;
         anim.SetTrigger("Die");
+        if(gameObject.name.Equals("IronAsteroid")){GameObject.FindWithTag("ScoreController").gameObject.GetComponent<ScoreSystem>().TakeScore(100 * ironKoef);}
+        if(gameObject.name.Equals("GoldAsteroid")){GameObject.FindWithTag("ScoreController").gameObject.GetComponent<ScoreSystem>().TakeScore(100 * goldKoef);}
+        if(gameObject.name.Equals("DiamondAsteroid")){GameObject.FindWithTag("ScoreController").gameObject.GetComponent<ScoreSystem>().TakeScore(100 * diamondKoef);}
+        
     }
 
     void Die()
