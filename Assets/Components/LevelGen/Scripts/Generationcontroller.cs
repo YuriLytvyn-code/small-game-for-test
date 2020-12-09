e7951cf4efb5bad8b48b66a4f295b793b21c34b8
senoron <perfectbynni@gmail.com>
2020-12-09 23:28:33 +0200

    Added exit button

---
+++ Assets/Components/LevelGen/Scripts/Generationcontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generationcontroller : MonoBehaviour
+{
+    [SerializeField] private GameObject[] AsteroidPrefabs;
+    [SerializeField] private int spawnRate = 3000;
+    void Start()
+    {
+        SpawnAsteroids(spawnRate);
+    }
+    private void SpawnAsteroids(int amount) 
+    {
+        for (int i=0; i < amount; i++) 
+        {
+            Vector3 pos = new Vector3(Random.Range(-300, 300), Random.Range(-300, 300), Random.Range(300, 300));
+            GameObject chosenAsteroid = AsteroidPrefabs[Random.Range(0, AsteroidPrefabs.Length)];
+            chosenAsteroid.name = "Asteroid";
+            GameObject asteroid = Instantiate(chosenAsteroid , pos, Quaternion.identity);
+            // You can still manipulate asteroid afterwards like .AddComponent etc
+        }
+    }
+}
---
+++ Assets/Components/LevelGen/Scripts/RestartBut.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartBut : MonoBehaviour
+{
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Generator");
+    }
+}
