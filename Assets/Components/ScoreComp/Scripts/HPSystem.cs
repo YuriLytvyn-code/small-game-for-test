2cb1394388107ab29c6044ca50955f08e1bb43f1
senoron <perfectbynni@gmail.com>
2020-12-09 18:20:15 +0200

    Added HP Script

---
+++ Assets/Components/ScoreComp/Scripts/HPSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPSystem : MonoBehaviour
+{
+    [SerializeField] private int HP = 0;
+    [SerializeField] private Text ScoreText;
+    
+    public void TakeHP(int HPcount)
+    {
+        if(HPcount < 0)
+        {
+            HPcount = 0;
+        }
+        HP += HPcount;
+        ScoreText.text = HP.ToString();
+    }
+}
